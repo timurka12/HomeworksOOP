a77464f18bcb5d956e8823981652c2fccbb32146
timurka12 <93543993+timurka12@users.noreply.github.com>
2022-03-21 21:12:50 +0300

    Add files via upload

---
+++ Program21.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp203
+{
+    enum AT { Current, Savings }
+    class Score
+    {
+        public Score(string accountNumber, decimal balans, AT type)
+        {
+            AccountNumber = accountNumber;
+            Balans = balans;
+            Type = type;
+        }
+        public string AccountNumber { get; set; }
+        public decimal Balans { get; set; }
+        public AT Type { get; set; }
+        public override string ToString() => $"AccountNumber - {AccountNumber}, Balans - {Balans}, Type - {Type}";
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+            AT accountType = AT.Current;
+            Console.WriteLine(accountType);
+            Score score = new Score("08052003NTR", 1_200_000, AT.Savings);
+            Console.WriteLine(score);
+        }
+
+    }
+}
